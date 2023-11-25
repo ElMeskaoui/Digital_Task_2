d7509843cdd5f2a072cd03cdac12ed8464107301
ElMeskaoui <elhoucine.elmeskaoui@gmail.com>
2023-11-25 01:47:03 +0100

    first commit

---
+++ src/main/java/com/example/digitaltask2/DigitalTask2Application.java
@@ -0,0 +1,13 @@
+package com.example.digitaltask2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalTask2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalTask2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/digitaltask2/DigitalTask2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitaltask2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalTask2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
